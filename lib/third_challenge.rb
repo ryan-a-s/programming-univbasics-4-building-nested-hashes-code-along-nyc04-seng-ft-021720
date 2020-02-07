6769f11c318a5f4cf514641eb53e6aa1d98cdbed
Ryan Seit <ryan@ryanseit.com>
2020-02-07 02:39:35 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -17,7 +17,18 @@ def third_challenge
         age: "15",
         status: "alive"
         },
-      :hero_friends => []
+      :hero_friends => [
+        {
+          name: "Benvolio",
+          age: "17",
+          attitude: "worried"
+        }
+        {
+          name: "Mercutio",
+          age: "18",
+          attitude: "hot-headed"
+        }
+      ]
    },
    :capulet => {
       :patriarch => {
@@ -36,7 +47,18 @@ def third_challenge
         age: "15",
         status: "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {
+          name: "Steven",
+          age: "30",
+          attitude: "confused"
+        }
+        {
+          name: "Nurse".
+          age: "44",
+          attitude: "worried"
+        }
+      ]
    }
   }
 
